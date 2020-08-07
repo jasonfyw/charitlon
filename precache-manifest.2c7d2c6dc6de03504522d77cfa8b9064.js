bac164aa8f884a6829705e845a1c8bc4714ba18a
Jason Wang <jasonwang0610@gmail.com>
2020-08-07 21:19:53 +0200

    Updates

---
+++ precache-manifest.2c7d2c6dc6de03504522d77cfa8b9064.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76145708a08263be1b882ec4828003af",
+    "url": "/charitlon/index.html"
+  },
+  {
+    "revision": "f4db4267db8856dc6c70",
+    "url": "/charitlon/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "757f948a047b4d145e6e",
+    "url": "/charitlon/static/css/main.30b52591.chunk.css"
+  },
+  {
+    "revision": "f4db4267db8856dc6c70",
+    "url": "/charitlon/static/js/2.d69dfd0e.chunk.js"
+  },
+  {
+    "revision": "8e87f68634b1e5b84f6f7c9aeab3aa68",
+    "url": "/charitlon/static/js/2.d69dfd0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "757f948a047b4d145e6e",
+    "url": "/charitlon/static/js/main.ee3d6547.chunk.js"
+  },
+  {
+    "revision": "e1bf52380c589d0ca63c",
+    "url": "/charitlon/static/js/runtime-main.fbef3195.js"
+  },
+  {
+    "revision": "acdbd465bfcd046a6d08eb4599ab7232",
+    "url": "/charitlon/static/media/charitlon-logo-heart.acdbd465.png"
+  },
+  {
+    "revision": "9c50cee43c28c66067244ba73e2eaf20",
+    "url": "/charitlon/static/media/charitlon-logo-palms.9c50cee4.png"
+  },
+  {
+    "revision": "1f061056272433f68f3d08609ea6257c",
+    "url": "/charitlon/static/media/dolnipocernice.1f061056.jpg"
+  },
+  {
+    "revision": "5f9d2453a73272ef7742d0370adff537",
+    "url": "/charitlon/static/media/nadace_preciosa_logo.5f9d2453.png"
+  },
+  {
+    "revision": "f523a1e80f5305478f3c67d1588f18b0",
+    "url": "/charitlon/static/media/renomia_logo.f523a1e8.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/charitlon/precache-manifest.2c7d2c6dc6de03504522d77cfa8b9064.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/charitlon/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
