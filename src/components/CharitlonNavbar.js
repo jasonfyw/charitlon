3a5b8eebfe8b567adb5fd50f072aa237ad8ef3ee
Jason Wang <jasonwang0610@gmail.com>
2020-08-09 16:55:41 +0200

    Update colour scheme with elements from Nord

--- src/components/CharitlonNavbar.js
+++ src/components/CharitlonNavbar.js
@@ -9,12 +9,12 @@ import { LogoInstagram, MailOutline } from '../media/Ionicons';
 
 const Link = styled(HashLink)`
     font-family: 'PT Mono', monospace;
-    color: #777;
+    color: ${props => props.collapsed === 'navbar-collapse' ? '#000' : 'var(--nord0)'};
     text-decoration: none;
-    transition: 0.3s;
+    transition: 0.2s;
 
     &:hover {
-        color: #333;
+        color: var(--nord3);
         text-decoration: none;
     }
 `;
@@ -47,11 +47,11 @@ class CharitlonNavbar extends Component {
 
         return (
             <Navbar fixed="top" className={navClass}>
-                <Navbar.Brand href="/">CHARITLON</Navbar.Brand>
+                <Navbar.Brand href="/"><span className="brand">CHARITLON</span></Navbar.Brand>
                 <Nav className="mr-auto">
-                    <Nav.Link><Link to="#about">About</Link></Nav.Link>
-                    <Nav.Link><Link to="#dolni-pocernice">Our Commitment</Link></Nav.Link>
-                    <Nav.Link><Link to="#pricing">Pricing</Link></Nav.Link>
+                    <Nav.Link><Link to="#about" collapsed={navClass} >About</Link></Nav.Link>
+                    <Nav.Link><Link to="#dolni-pocernice" collapsed={navClass}>Our Commitment</Link></Nav.Link>
+                    <Nav.Link><Link to="#pricing" collapsed={navClass}>Pricing</Link></Nav.Link>
                 </Nav>
                 
                 <Nav>
